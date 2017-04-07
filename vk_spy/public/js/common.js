c5fbe09f18d0cb04c0c16cf3b29f016b95e1fab5
Qu1ckson <quick625@yandex.ru>
2017-04-07 21:38:08 +0300

    макет старт

---
+++ vk_spy/public/js/common.js
@@ -0,0 +1,68 @@
+'use strict';
+
+$(".account-selector-menu").css("display", "block");
+$(".nano").nanoScroller();
+$(".account-selector-menu").css("display", "none");
+
+var totalHeight = 0;
+$("div.message-wrap").each(function(index){
+  totalHeight += parseInt($(this).height(), 10);
+});
+
+if(totalHeight >= 666){
+  $(".im-history").css("justify-content", "none");
+  $(".im-history-wrapper .nano").nanoScroller({ scroll: 'bottom' });
+} else {
+  $(".im-history").css("justify-content", "flex-end");
+}
+
+var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+
+elems.forEach(function(html) {
+  var switchery = new Switchery(html, { size: "small", color: "#479" });
+});
+
+
+$(".friends a").on("click", function(event){
+  $(".friends li.selected").removeClass("selected");
+  $(event.target).closest("li").addClass("selected");
+});
+
+$(".dialog-menu nav a.option").on("click", function(event){
+  var inputList = $(event.target).children(".js-switch");
+  if(inputList.length === 0)
+    return;
+  var input = inputList[0];
+  if(input.checked){
+    input.checked = false;
+    onChange(input);
+  } else {
+    input.checked = true;
+    onChange(input);
+  }
+});
+
+function onChange(el) {
+    if (typeof Event === 'function' || !document.fireEvent) {
+        var event = document.createEvent('HTMLEvents');
+        event.initEvent('change', true, true);
+        el.dispatchEvent(event);
+    } else {
+        el.fireEvent('onchange');
+    }
+};
+
+$(".account-selector-menu ul li a").on("click", function(event){
+  $(".account-menu li.selected").removeClass("selected");
+  $(event.target).closest("li").addClass("selected");
+});
+
+$("#openUserAccounts").on("click", function(event){
+  if($("#openUserAccounts").hasClass("is-active")){
+    $("#openUserAccounts").removeClass("is-active");
+    $(".account-selector-menu").css("display", "none");
+  } else {
+    $("#openUserAccounts").addClass("is-active");
+    $(".account-selector-menu").css("display", "block");
+  }
+});
---
+++ vk_spy/public/libs/menu-button/menu-button.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var toggles = document.querySelectorAll(".c-hamburger");
+
+for (var i = toggles.length - 1; i >= 0; i--) {
+  var toggle = toggles[i];
+  toggleHandler(toggle);
+};
+
+function toggleHandler(toggle) {
+  toggle.addEventListener( "click", function(e) {
+    e.preventDefault();
+
+    var menuColor = "#58a";
+    var darkMenuColor = "#479";
+
+    var hideMenuHandler = function(element){
+      element.classList.remove("is-active");
+      $(".dialog-menu nav").css("display", "none");
+    };
+
+    var openMenuHandler = function(element){
+      element.classList.add("is-active");
+      $(".dialog-menu nav").css("display", "block");
+    };
+
+    (this.classList.contains("is-active") === true) ? hideMenuHandler(this): openMenuHandler(this);
+  });
+}
